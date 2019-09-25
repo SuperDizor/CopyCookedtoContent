7974ab41726a78f3e95ff404a17330c963095bdd
SuperDizor <felixdion25@hotmail.com>
2019-09-24 23:16:43 -0400

    first release

---
+++ copyCooked.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+
+:: Source from the Cooked folder
+SET "cookedContent=C:\Users\SuperDizor\Documents\Unreal Projects\Session_DizorPark\Saved\Cooked\WindowsNoEditor\Session_DizorPark\Content"
+:: Destination to Session Content folder
+SET "sessionContent=D:\Session_Stuffs\Session\SessionGame\Content"
+
+
+:: Do not modify below unless you know what you do.
+ECHO Start copying folders/files...
+ECHO.
+robocopy "%cookedContent%" "%sessionContent%" /E /IS /XD Data\
+ECHO.
+ECHO ------------------------------------------------------------------------------
+ECHO Script by SuperDizor#0005 | https://github.com/SuperDizor/CopyCookedtoContent
+PAUSE
